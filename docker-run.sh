d1539a8cd843a61172a06f06376c1d43ee2b0dc9
krsuhjunho <49592845+krsuhjunho@users.noreply.github.com>
2021-10-12 13:18:34 +0900

    Create docker-run.sh

---
+++ docker-run.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+#VAR
+DOCKER_CONTAINER_NAME="groupsession"
+CONTAINER_HOST_NAME="groupsession"
+TOMCAT_PORT=8080
+BASE_IMAGE_NAME="ghcr.io/krsuhjunho/almalinux8-groupsession"
+SERVER_IP=$(curl -s ifconfig.me)
+PC_URL="gsession/"
+HTTP_BASE="http://"
+TODAY=$(date "+%Y-%m-%d")
+TIME_ZONE="Asia/Tokyo"
+COMMIT_COMMENT="$2"
+BUILD_OPTION="$1"
+
+DOCKER_IMAGE_BUILD()
+{
+docker build -t ${BASE_IMAGE_NAME} .
+}
+
+DOCKER_IMAGE_PUSH()
+{
+docker push ${BASE_IMAGE_NAME}
+}
+
+GIT_COMMIT_PUSH()
+{
+git add -u
+git commit -a -m "${TODAY} ${COMMIT_COMMENT}"
+git config credential.helper store
+git push origin main
+}
+
+DOCKER_CONTAINER_REMOVE()
+{
+docker rm -f ${DOCKER_CONTAINER_NAME}
+}
+
+DOCKER_CONTAINER_CREATE()
+{
+docker run -tid --privileged=true \
+-h "${CONTAINER_HOST_NAME}" \
+--name="${DOCKER_CONTAINER_NAME}" \
+-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+-v /etc/localtime:/etc/localtime:ro \
+-e TZ=${TIME_ZONE} \
+-p ${TOMCAT_PORT}:8080 \
+--restart=always \
+${BASE_IMAGE_NAME}
+
+}
+
+DOCKER_CONTAINER_BASH()
+{
+docker exec -it ${DOCKER_CONTAINER_NAME} /bin/bash /usr/local/src/RUN-INIT.sh
+}
+
+DOCKER_CONTAINER_URL_SHOW()
+{
+echo ""
+echo "GroupSession URL => ${HTTP_BASE}${SERVER_IP}:${TOMCAT_PORT}/${PC_URL}"
+echo ""
+}
+
+MAIN()
+{
+
+if [ "$BUILD_OPTION" == "--build" ]; then
+    DOCKER_IMAGE_BUILD
+	#DOCKER_IMAGE_PUSH
+	#GIT_COMMIT_PUSH
+fi
+
+DOCKER_CONTAINER_REMOVE
+DOCKER_CONTAINER_CREATE
+DOCKER_CONTAINER_BASH
+DOCKER_CONTAINER_URL_SHOW
+}
+
+MAIN
